dad61cdc61d3093280e863becb303935523e4eba
Leo Yang <leo19941227@gmail.com>
2024-06-11 00:36:07 +0800

    update shell

--- shells/filter_10_90.sh
+++ shells/filter_10_90.sh
@@ -5,6 +5,6 @@ set -e
 
 for iteration in 10 50 100 150 200;
 do
-    python3 dino_boosting.py submissions/filter_10_90/iteration_${iteration} --n_iter ${iteration} --filter_outlier --filter_low 0.101 --filter_high 0.901
+    python3 dino_boosting.py submissions/filter_10_90/iteration_${iteration} --n_iter ${iteration} --filter_outlier --filter_low 0.102 --filter_high 0.902
 done
 
